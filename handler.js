377449d9ae4029e0c62cd256ee64dbc4ba299ed0
Eder Velazquez <edervzz.work@gmail.com>
2022-04-26 15:58:25 -0500

    asd

--- handler.js
+++ handler.js
@@ -1,17 +1,19 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  return {
+  let r = {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Hola ${event.pathParameters.name}`,
         input: event,
       },
       null,
       2
     ),
-  };
+  }
+  console.log(r)
+  return r;
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
