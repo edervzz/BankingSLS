a28cff6f0c9dcc25fc8f6b7a1c9812286ebe8307
edervzz <edervzz.work@gmail.com>
2022-05-01 00:39:41 -0500

    Sanity Check

--- sanityCheck.js
+++ sanityCheck.js
@@ -5,12 +5,31 @@ const bodyParser = require("body-parser");
 
 
 module.exports.hello = async (event) => {
+  console.log(event);
   let r = {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: `Hola ${event.pathParameters.name}`,
-        input: body.name,
+        input: event,
+      },
+      null,
+      2
+    ),
+  }
+  console.log(r)
+  return r;
+
+};
+
+module.exports.greet = async (event) => {
+  let body = JSON.parse(event.body)
+  let r = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hola ${body.name}`,
+        input: body,
       },
       null,
       2
